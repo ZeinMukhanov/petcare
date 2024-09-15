b6393af1cbbc2d751bb7b43920351bf2a925678e
ZeinMukhanov <zm2199@nyu.edu>
2024-09-16 03:41:25 +0400

    adding .auth to NextAuth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import { nextAuthEdgeConfig } from "./lib/auth-edge";
 
-export default NextAuth(nextAuthEdgeConfig);
+export default NextAuth(nextAuthEdgeConfig).auth;
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
